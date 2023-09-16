335403370d4d68a3b9047bc49e48a2d69e11d27e
MaksimKanevskiy <145094580+MaksimKanevskiy@users.noreply.github.com>
2023-09-16 18:56:53 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
+	if a > b {
+		if a > c {
+			fmt.Println(a)
+		} else {
+			fmt.Println(c)
+		}
+	} else {
+		if b > c {
+			fmt.Println(b)
+		} else {
+			fmt.Println(c)
+		}
+	}
+
+	var input string
+	fmt.Scanf("%v", &input)
+	fmt.Scanf(" ")
+}
